bd716b7f48efc31ee118d33448a533a5772c6321
Andrii <whiteyelnikov@gmail.com>
2024-09-12 15:23:35 +0300

    added fonts, dropdowns in header

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,17 @@ toggleButton.addEventListener('click', () => {
   headerList.classList.toggle('active');
   toggleButton.classList.toggle('active');
 });
+
+// header list svg plus icon to open a dropdown
+
+const svgPlusArr = document.querySelectorAll('.header-list__svg');
+
+for (let i = 0; i < svgPlusArr.length; i++) {
+  svgPlusArr[i].addEventListener('click', () => {
+    
+    const listItem = svgPlusArr[i].closest('.header-list__item');
+    listItem.classList.toggle('active');
+    const dropdownEl = listItem.querySelector('.dropdown');
+    dropdownEl.classList.toggle('active');
+  })
+}
